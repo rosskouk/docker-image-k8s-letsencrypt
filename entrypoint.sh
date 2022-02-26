76bd1716086212c1bc2941c918385f1505f00b74
Ross Stewart <rosskouk@gmail.com>
2022-02-26 18:20:02 +0000

    - Move files out of /

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,14 +18,14 @@ CERTPATH=/etc/letsencrypt/live/$(echo $DOMAINS | cut -f1 -d',')
 
 ls $CERTPATH || exit 1
 
-cat /secret-patch-template.json | \
+cat $HOME/secret-patch-template.json | \
 	sed "s/NAMESPACE/${NAMESPACE}/" | \
 	sed "s/NAME/${SECRET}/" | \
 	sed "s/TLSCERT/$(cat ${CERTPATH}/fullchain.pem | base64 | tr -d '\n')/" | \
 	sed "s/TLSKEY/$(cat ${CERTPATH}/privkey.pem |  base64 | tr -d '\n')/" \
-	> /secret-patch.json
+	> $HOME/secret-patch.json
 
-ls /secret-patch.json || exit 1
+cat $HOME/secret-patch.json || exit 1
 
 # update secret
-curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
+curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @$HOME/secret-patch.json https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
