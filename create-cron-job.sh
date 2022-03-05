f8af72a6806626c5b94e96ae7e96d219ff6cbf09
Ross Stewart <rosskouk@gmail.com>
2022-03-05 18:14:25 +0000

    - Updated scripts

--- create-cron-job.sh
+++ create-cron-job.sh
@@ -1,25 +1,71 @@
 #!/bin/bash
 
-if [[ -z $EMAIL || -z $DOMAINS || -z $SECRET || -z $JOB_NAME]]; then
-	echo "EMAIL, DOMAINS, SECRET and JOB_NAME env vars are required!"
-	env
-	exit 1
-fi
-
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 
+# Check if cronjob already exists
 
-#sed "s/TPL_NAMESPACE/${NAMESPACE}/" | \
+job_status=$(curl -sk --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+	              -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+		          -X GET https://kubernetes/apis/batch/v1/namespaces/${NAMESPACE}/cronjobs/${SECRET}-cronjob/ | \
+		     grep code | \
+		     awk -F ':' '{print $2}' | \
+		     xargs
+	        )
 
-cat $HOME/tpl-cron-job.json | \
-	sed "s/TPL_CRON_NAME/${JOB_NAME}/" | \
-	sed "s/TPL_CRON_CRONTAINER_NAME/${SECRET}-cronjob"
+if [ -z $job_status ]
+then
+	echo "Job already exists, nothing to do"
+	exit 0
+
+elif [ $job_status -eq 404 ]
+then
+	if [[ -z $EMAIL || -z $DOMAINS || -z $SECRET ]]
+	then
+		echo "EMAIL, DOMAINS and SECRET env vars are required!"
+		env
+		exit 1
+	fi
+
+	echo "Cron job does not exist, creating...."
+
+	# Fill in template
+	cat $HOME/tpl-cron-job.json | \
+	sed "s/TPL_CRON_NAME/${SECRET}-cronjob/" | \
+	sed "s/TPL_CRON_CRONTAINER_NAME/${SECRET}-certbot/" | \
 	sed "s/TPL_DOMAINS/${DOMAINS}/" | \
 	sed "s/TPL_EMAIL/${EMAIL}/" | \
 	sed "s/TPL_SECRET_NAME/${SECRET}/" \
 	> $HOME/job-patch.json
 
-cat $HOME/job-patch.json || exit 1
+	# Create the cron job
+	job_create=$(curl -sk --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+			          -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+					  -H "Content-Type: application/json" \
+					  -d @$HOME/job-patch.json \
+					  -X POST https://kubernetes/apis/batch/v1/namespaces/${NAMESPACE}/cronjobs/ | \
+				 grep code | \
+		         awk -F ':' '{print $2}' | \
+		         xargs
+				)
 
-# Create cron job
-#curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @$HOME/secret-patch.json https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
+	# Ensure $job_create is NULL
+	if [ -z $job_create ]
+	then
+		echo "Cron job created"
+		exit 0
+
+	else
+		echo "An error occurred while creating the cron job"
+		exit 1
+	fi
+
+else
+	echo "An error occurred while checking if the cronjob exists"
+	exit 1
+fi
+
+# Create job from cronjob
+
+# Check completion
+
+# Delete one time job
--- create-secret.sh
+++ create-secret.sh
@@ -31,7 +31,25 @@ cat $HOME/tpl-secret.json | \
 	sed "s/TPL_TLS_KEY/$(cat ${CERTPATH}/privkey.pem |  base64 | tr -d '\n')/" \
 	> $HOME/secret-patch.json
 
-cat $HOME/secret-patch.json || exit 1
-
 # update secret
-curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @$HOME/secret-patch.json https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
+secret_update=$(curl -sk --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+					 -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+					 -H "Accept: application/json, */*" \
+					 -H "Content-Type: application/strategic-merge-patch+json" \
+					 -d @$HOME/secret-patch.json \
+					 -X PATCH https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET} | \
+				grep code | \
+		        awk -F ':' '{print $2}' | \
+		        xargs
+			   )
+
+# Ensure $secret_update is NULL
+if [ -z $secret_update ]
+then
+	echo "Secret updated"
+	exit 0
+
+else
+	echo "An error occurred while updating the secret"
+	exit 1
+fi
