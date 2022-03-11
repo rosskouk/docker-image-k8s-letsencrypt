adbfe14c7aabacc0800627e40dd5118403337a96
Ross Stewart <rosskouk@gmail.com>
2022-03-11 13:42:31 +0000

    - Renamed job_create to cron_job_create

--- create-cron-job.sh
+++ create-cron-job.sh
@@ -4,13 +4,13 @@ NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 
 # Check if cronjob already exists
 
-job_status=$(curl -sk --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
-	              -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-		          -X GET https://kubernetes/apis/batch/v1/namespaces/${NAMESPACE}/cronjobs/${SECRET}-cronjob/ | \
-		     grep code | \
-		     awk -F ':' '{print $2}' | \
-		     xargs
-	        )
+cron_job_status=$(curl -sk --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+	            	   -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+		               -X GET https://kubernetes/apis/batch/v1/namespaces/${NAMESPACE}/cronjobs/${SECRET}-cronjob/ | \
+		     	  grep code | \
+		     	  awk -F ':' '{print $2}' | \
+		     	  xargs
+	        	)
 
 if [[ -z $cron_job_status ]]
 then
@@ -28,34 +28,34 @@ then
 
 	echo "Cron job does not exist, creating...."
 
-	# Fill in template
+	# Fill in the cron job template
 	cat $HOME/tpl-cron-job.json | \
 	sed "s/TPL_CRON_NAME/${SECRET}-cronjob/" | \
-	sed "s/TPL_CRON_CRONTAINER_NAME/${SECRET}-certbot/" | \
+	sed "s/TPL_CRON_CONTAINER_NAME/${SECRET}-certbot/" | \
 	sed "s/TPL_DOMAINS/${DOMAINS}/" | \
 	sed "s/TPL_EMAIL/${EMAIL}/" | \
 	sed "s/TPL_SECRET_NAME/${SECRET}/" \
-	> $HOME/job-patch.json
+	> $HOME/cron-job-patch.json
 
 	# Create the cron job
-	job_create=$(curl -sk --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
-			          -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-					  -H "Content-Type: application/json" \
-					  -d @$HOME/job-patch.json \
-					  -X POST https://kubernetes/apis/batch/v1/namespaces/${NAMESPACE}/cronjobs/ | \
-				 grep code | \
-		         awk -F ':' '{print $2}' | \
-		         xargs
-				)
+	cron_job_create=$(curl -sk --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+			               -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+					       -H "Content-Type: application/json" \
+					       -d @$HOME/cron-job-patch.json \
+					       -X POST https://kubernetes/apis/batch/v1/namespaces/${NAMESPACE}/cronjobs/ | \
+				      grep code | \
+		              awk -F ':' '{print $2}' | \
+		              xargs
+				    )
 
+	# Ensure $cron_job_create is NULL
 	if [[ -z $cron_job_create ]]
 	then
 		echo "Cron job created"
-		exit 0
 
 	else
 		echo "An error occurred while creating the cron job"
-		echo $job_create
+		echo $cron_job_create
 		exit 1
 	fi
 
